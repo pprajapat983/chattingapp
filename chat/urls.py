e975b04aa9998ee3931f70482a712acaddb32fc1
davidjohnson1196 <72160760+davidjohnson1196@users.noreply.github.com>
2020-10-01 18:15:36 +0530

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('accounts/',views.accounts,name="account"),
+    path('realaccounts/',views.realaccounts,name="realaccount"),
 ]
